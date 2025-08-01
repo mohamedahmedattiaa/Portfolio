bcff684aef16809b31232eaef8ee4799660e1980
mohamedaahmedattia <mohamedahmedattia27@gmail.com>
2025-08-01 16:32:49 +0300

    contactus problem try to solve it

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  emailServiceID: '',
-  emailTemplateID: '',
-  emailPublicKey: ''
+  emailServiceID: 'service_lcy36id',
+  emailTemplateID: 'template_5gqmfrd',
+  emailPublicKey: 'YEuBlGH7igYjJpI83'
 };
